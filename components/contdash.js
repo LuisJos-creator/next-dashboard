13614f12978008beab5270182a28f3b9d266d87b
LuisJos-creator <69698670+LuisJos-creator@users.noreply.github.com>
2026-02-16 22:42:00 -0400

    #

--- components/contdash.js
+++ components/contdash.js
@@ -2,32 +2,42 @@ import React from "react";
 import Radial from "./radial";
 import Progress from "./progress";
 import Stats from "./stats";
+import { supabase } from "../utils/supabaseClient";
 
 const contdash = () => {
+  
+const fetchData = async () => {
+  const { data, error } = await supabase
+    .from("inventario")
+    .select("*");
+  if (error) {
+    console.error("Error fetching data:", error);
+  } else {
+    console.log("Data fetched successfully:", data);
+  }
+};
+
+fetchData();
+
   return (
     <>
       <div className="grid grid-cols-4 grid-rows-3 gap-4">
         <div className="bg-base-200 rounded-box p-3 m-3 row-span-2">
           <div className="flex flex-col items-center justify-center h-full">
             <Radial />
-            <h1 className="mt-2">Valor</h1>
+            <h1 className="mt-2">Porcentaje de stock</h1>
           </div>
         </div>
         <div className="bg-base-200 rounded-box p-3 m-3 row-span-1 col-start-2 row-start-2">
           <div className="flex flex-col items-center justify-center h-full">
             <Progress />
-            <h1 className="mt-2">Valor</h1>
+            <h1 className="mt-2">Productos en stock</h1>
           </div>
         </div>
         <div className="bg-base-200 rounded-box p-3 m-3 row-span-2 col-start-3 row-start-1">
           <div className="flex flex-col items-center justify-center h-full">
             <Radial />
-            <h1 className="mt-2">Valor</h1>
-          </div>
-        </div>
-        <div className="bg-base-200 rounded-box p-3 m-3 row-span-2 col-start-4 row-start-2">
-          <div className="flex flex-col items-center justify-center h-full">
-            <Stats />
+            <h1 className="mt-2">Porcentaje de ventas</h1>
           </div>
         </div>
         <div className="bg-base-200 rounded-box p-3 m-3 row-span-2 col-start-4 row-start-1">
@@ -38,7 +48,7 @@ const contdash = () => {
         <div className="bg-base-200 rounded-box p-3 m-3 row-span-1 col-start-2 row-start-1">
           <div className="flex flex-col items-center justify-center h-full">
             <Progress />
-            <h1 className="mt-2">Valor</h1>
+            <h1 className="mt-2">Objetivo de ventas</h1>
           </div>
         </div>
       </div>
